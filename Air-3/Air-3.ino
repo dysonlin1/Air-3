97ab47748762ee3666623c3de694aabf16a86a20
林湧森 (Dyson Lin) <dysonlin@gmail.com>
2021-05-30 17:18:56 +0800

    Update Air-3.ino

--- Air-3/Air-3.ino
+++ Air-3/Air-3.ino
@@ -1,7 +1,7 @@
-// Air-1
+// Air-3
 //
-// Read the air voltage of (A0 - A1) to predict quakes
-// 測量(A0 - A1)的空氣電壓以預測地震
+// Read the air voltage of A0, A1, and (A0 - A1) to predict quakes
+// 測量A0、A1和(A0 - A1)的空氣電壓以預測地震
 // 
 // 台灣地震預測研究所 所長 
 // 林湧森
@@ -13,6 +13,7 @@
 //                             Change output from raw data to mV
 // 2016-12-18 22:21 UTC+8 V1.3 Change program name to Air-2
 // 2019-11-07 16:28 UTC+8 V1.4 Modidy Air-2 to get Air-1
+// 2021-05-30 17:15 UTC+8 V1.5 Modidy Air-1 to get Air-3
 
 // analogRead() returns 0 ~ 1023
 // 0 means 0 mV.
@@ -25,12 +26,19 @@ void setup()
 
 void loop()
 {
-  int valueA0 = analogRead(A0);
-  int valueA1 = analogRead(A1);
+  int A0_value = analogRead(A0);
+  int A1_value = analogRead(A1);
+  // int A0_A1_value = A0_value - A1_value;
   
   // Convert 10 bit ADC value 0 ~ 1023 to 0 mV ~ 5000 mV
-  int mV = round(((valueA0 - valueA1) * 5000.0 ) / 1023.0);
+  int A0_mV = round((A0_value * 5000.0 ) / 1023.0);
+  int A1_mV = round((A1_value * 5000.0 ) / 1023.0);
+  // int A0_A1_mV = round(((A0_value - A1_value) * 5000.0 ) / 1023.0);
   
-  Serial.println(mV);
+  // Serial.println(A0_A1_mV);
+  Serial.println("A0_mV");
+  Serial.println(A0_mV);
+  Serial.println("A1_mV");
+  Serial.println(A1_mV);
   delay(200);
 }
